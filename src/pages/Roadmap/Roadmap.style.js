d2a11a438e15d160cc8d60f3c6ed57465e791faf
ifanshx <irfaanshoodiq1954@gmail.com>
2023-03-07 18:43:26 +0700

    first commit

--- src/pages/Roadmap/Roadmap.style.js
+++ src/pages/Roadmap/Roadmap.style.js
@@ -1,41 +1,198 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const SectionRoadmap = styled.div`
   max-height: 100vh;
   height: 100vh;
   width: 100%;
-  background-color: #e2ebec;
+  background-color: #fff7ed;
   display: flex;
   justify-content: center;
   align-items: center;
   overflow-y: hidden;
 `;
 
-export const Box = styled.div`
-  height: 206px;
-  width: 280px;
+const CloudsFly = keyframes`
+  to{ transform:translateY(20px); }
+`;
+export const Clouds3 = styled.img`
+  width: 100%;
+  animation: ${CloudsFly} 3s infinite alternate;
+  position: fixed;
+  pointer-events: none;
+`;
+
+const CloudsFrontFly = keyframes`
+  to{ transform:translateY(5px); }
+`;
+export const CloudsFront3 = styled.img`
+  width: 102%;
+  height: 106%;
+  animation: ${CloudsFrontFly} 4s infinite alternate;
+  position: fixed;
+  z-index: 998;
+  pointer-events: none;
+  @media (max-width: 1172px) {
+    width: 390%;
+    width: 392%;
+    animation: ${CloudsFrontFly} 1s infinite alternate;
+  }
+`;
+
+export const Roadmaps = styled.img`
+  display: flex;
+  position: fixed;
+  bottom: 75%;
+  width: 25%;
+`;
+
+export const Box1 = styled.div`
+  height: 250px;
+  width: 300px;
   position: absolute;
   overflow: hidden;
   cursor: pointer;
-  border-radius: 5px;
-  bottom: 6%;
-  right: 6%;
+  border-radius: 1px;
+  bottom: 45%;
+  right: 70%;
   padding: 1px 1px;
-  background-color: #c9a27522;
+  background-color: #4b6f70;
   transition: all 0.4s ease 0s;
-  transform: translate3d(10%, 0, 0) scale(1.3);
+  transform: translate3d(10%, 0, 0) scale(1.1);
   &:hover {
+    z-index: 999;
     border: 2px solid;
-    border-radius: 30px 0px;
-    padding: 5px 5px;
+    border-radius: 10px 0px 0px 10px;
+    padding: 0px 20px;
   }
 `;
 
 export const Hero1 = styled.img`
   box-sizing: border-box;
   display: flex;
+  width: 85%;
+  margin: 1px 28%;
+  &:hover {
+    transform: translate3d(10%, 5%, 0) scale(1.1);
+  }
+`;
+
+export const Box2 = styled.div`
+  height: 250px;
+  width: 300px;
+  position: absolute;
+  overflow: hidden;
+  cursor: pointer;
+  border-radius: 1px;
+  bottom: 4%;
+  right: 70%;
+  padding: 1px 1px;
+  background-color: #6d333f;
+  transition: all 0.4s ease 0s;
+  transform: translate3d(10%, 0, 0) scale(1.1);
+  &:hover {
+    z-index: 999;
+    border: 2px solid;
+    border-radius: 10px 0px 0px 10px;
+    padding: 0px 20px;
+  }
+`;
+
+export const Hero2 = styled.img`
+  box-sizing: border-box;
+  display: flex;
+  width: 85%;
+  margin: 1px 28%;
+  &:hover {
+    transform: translate3d(10%, 5%, 0) scale(1.1);
+  }
+`;
+export const Box3 = styled.div`
+  height: 400px;
+  width: 300px;
+  position: absolute;
+  overflow: hidden;
+  cursor: pointer;
+  border-radius: 1px;
+  bottom: 15%;
+  right: 42%;
+  padding: 1px 1px;
+  background-color: #8d755c;
+  transition: all 0.4s ease 0s;
+  transform: translate3d(10%, 0, 0) scale(1.1);
+  &:hover {
+    z-index: 999;
+    border: 2px solid;
+    border-radius: 10px 10px 10px 10px;
+    padding: 10px 0px 0px 10px;
+  }
+`;
+
+export const Hero3 = styled.img`
+  box-sizing: border-box;
+  display: flex;
   width: 100%;
-  margin: 75px 28%;
+  margin: 120px 1%;
+  &:hover {
+    transform: translate3d(10%, 5%, 0) scale(1.1);
+  }
+`;
+export const Box4 = styled.div`
+  height: 250px;
+  width: 300px;
+  position: absolute;
+  overflow: hidden;
+  cursor: pointer;
+  border-radius: 1px;
+  bottom: 4%;
+  right: 14%;
+  padding: 1px 1px;
+  background-color: #ffaa67;
+  transition: all 0.4s ease 0s;
+  transform: translate3d(10%, 0, 0) scale(1.1);
+  &:hover {
+    z-index: 999;
+    border: 2px solid;
+    border-radius: 0px 10px 10px 0px;
+    padding: 0px 20px;
+  }
+`;
+
+export const Hero4 = styled.img`
+  box-sizing: border-box;
+  display: flex;
+  width: 90%;
+  margin: 1px 10%;
+  &:hover {
+    transform: translate3d(10%, 5%, 0) scale(1.1);
+  }
+`;
+
+export const Box5 = styled.div`
+  height: 250px;
+  width: 300px;
+  position: absolute;
+  overflow: hidden;
+  cursor: pointer;
+  border-radius: 1px;
+  bottom: 45%;
+  right: 14%;
+  padding: 1px 1px;
+  background-color: #250;
+  transition: all 0.4s ease 0s;
+  transform: translate3d(10%, 0, 0) scale(1.1);
+  &:hover {
+    z-index: 999;
+    border: 2px solid;
+    border-radius: 0px 10px 10px 0px;
+    padding: 0px 20px;
+  }
+`;
+
+export const Hero5 = styled.img`
+  box-sizing: border-box;
+  display: flex;
+  width: 85%;
+  margin: 1px 28%;
   &:hover {
     transform: translate3d(10%, 5%, 0) scale(1.1);
   }
--- src/pages/Roadmap/index.js
+++ src/pages/Roadmap/index.js
@@ -1,12 +1,55 @@
 import React from "react";
-import { Box, Hero1, SectionRoadmap } from "./Roadmap.style";
-import Heros1 from "../../assets/image/hero-1.png";
+import {
+  Box1,
+  Box2,
+  Box3,
+  Box4,
+  Box5,
+  Clouds3,
+  CloudsFront3,
+  Hero1,
+  Hero2,
+  Hero3,
+  Hero4,
+  Hero5,
+  Roadmaps,
+  SectionRoadmap,
+} from "./Roadmap.style";
+import cloudsFront from "../../assets/image/clouds_front.png";
+import clouds1 from "../../assets/image/clouds_back.png";
+import Roadmapss from "../../assets/image/roadmap.png";
+import Heros1 from "../../assets/image/Hero1.png";
+import Heros2 from "../../assets/image/Hero2.png";
+import Heros3 from "../../assets/image/Hero3.png";
+import Heros4 from "../../assets/image/Hero4.png";
+import Heros5 from "../../assets/image/Hero5.png";
 const Roadmap = () => {
   return (
     <SectionRoadmap>
-      <Box>
+      <CloudsFront3 src={cloudsFront} />
+      <Clouds3 src={clouds1} />
+      <Roadmaps src={Roadmapss} />
+      <Box1>
+        <div>Chapter I.</div>
         <Hero1 src={Heros1} />
-      </Box>
+      </Box1>
+      <Box2>
+        {" "}
+        <div>Chapter II.</div>
+        <Hero2 src={Heros2} />
+      </Box2>{" "}
+      <Box3>
+        <div>Chapter III.</div>
+        <Hero3 src={Heros3} />
+      </Box3>{" "}
+      <Box4>
+        <div>Chapter IV.</div>
+        <Hero4 src={Heros4} />
+      </Box4>{" "}
+      <Box5>
+        <div>Chapter V.</div>
+        <Hero5 src={Heros5} />
+      </Box5>
     </SectionRoadmap>
   );
 };
